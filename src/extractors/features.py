3b81dc94dca4c61e1f4624b1352195b37a377702
Neriya Mazzuz <neriya92@gmail.com>
2022-02-15 18:58:36 +0200

    Save changes

--- src/extractors/features.py
+++ src/extractors/features.py
@@ -27,6 +27,18 @@ def extract_server_name(session):
 
 
 def extract_features(session):
+    direction_features = extract_direction_features(session)
+    time_features = extract_time_features(session)
+    metadata_features = extract_metadata_features(session)
+    features = [*direction_features, *time_features, *metadata_features]
+    if len(features) != 2007:
+        print("The len is: " + str(len(features)))
+        exit(1)
+
+    return features
+
+
+def extract_direction_features(session):
     features = []
     src = session[0][IP].src
     for pkt in session:
@@ -41,3 +53,36 @@ def extract_features(session):
         features.extend([0] * (1000 - len(features)))
 
     return features
+
+
+def extract_time_features(session):
+    features = []
+    prev_time = session[0].time
+    for pkt in session[1:]:
+        if len(features) == 1000:
+            break
+        curr_time = pkt.time
+        features.append(curr_time - prev_time)
+        prev_time = curr_time
+
+    if len(features) < 1000:
+        features.extend([0] * (1000 - len(features)))
+
+    return features
+
+
+def extract_metadata_features(session):
+    total_packets = len(session)
+    total_incoming = 0
+    total_outgoing = 0
+    total_time = session[-1].time - session[0].time
+    src = session[0][IP].src
+    for pkt in session:
+        if pkt[IP].src == src:
+            total_outgoing = total_outgoing + 1
+        else:
+            total_incoming = total_incoming + 1
+    return [total_packets, total_incoming, total_outgoing,
+            total_incoming / total_packets,
+            total_outgoing / total_packets,
+            total_time, total_time / total_packets]
